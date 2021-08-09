c733440f589b56bce443f0b03210f6482d4f7618
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2021-08-09 13:07:20 -0700

    Changes to .gitignore

--- affix-returner/affix-returner/main.cpp
+++ affix-returner/affix-returner/main.cpp
@@ -1,166 +1,72 @@
-#pragma once
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
-#include <string.h>
-#include <stdio.h>
-#include <openssl/rsa.h>
-#include <openssl/pem.h>
-#include <openssl/engine.h>
+#include <iostream>
+#include <iomanip>
 
+#include "modes.h"
+#include "aes.h"
+#include "filters.h"
 
-void handleErrors(void) {
-    ERR_print_errors_fp(stderr);
-    abort();
-}
+int main(int argc, char* argv[]) {
 
-int encrypt(unsigned char* plaintext, int plaintext_len, unsigned char* key,
-    unsigned char* iv, unsigned char* ciphertext) {
-    EVP_CIPHER_CTX* ctx;
+    //Key and IV setup
+    //AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-   
+    //bit). This key is secretly exchanged between two parties before communication   
+    //begins. DEFAULT_KEYLENGTH= 16 bytes
+    CryptoPP::byte key[CryptoPP::AES::DEFAULT_KEYLENGTH], iv[CryptoPP::AES::BLOCKSIZE];
+    memset(key, 0x00, CryptoPP::AES::DEFAULT_KEYLENGTH);
+    memset(iv, 0x00, CryptoPP::AES::BLOCKSIZE);
 
-    int len;
+    //
+    // String and Sink setup
+    //
+    std::string plaintext = "Now is the time for all good men to come to the aide...";
+    std::string ciphertext;
+    std::string decryptedtext;
 
-    int ciphertext_len;
+    //
+    // Dump Plain Text
+    //
+    std::cout << "Plain Text (" << plaintext.size() << " bytes)" << std::endl;
+    std::cout << plaintext;
+    std::cout << std::endl << std::endl;
 
-    /* Create and initialise the context */
-    if (!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
+    //
+    // Create Cipher Text
+    //
+    CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
+    CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption(aesEncryption, iv);
 
-    /*
-     * Initialise the encryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
-    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-        handleErrors();
+    CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink(ciphertext));
+    stfEncryptor.Put(reinterpret_cast<const unsigned char*>(plaintext.c_str()), plaintext.length());
+    stfEncryptor.MessageEnd();
 
-    /*
-     * Provide the message to be encrypted, and obtain the encrypted output.
-     * EVP_EncryptUpdate can be called multiple times if necessary
-     */
-    if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-        handleErrors();
-    ciphertext_len = len;
+    //
+    // Dump Cipher Text
+    //
+    std::cout << "Cipher Text (" << ciphertext.size() << " bytes)" << std::endl;
 
-    /*
-     * Finalise the encryption. Further ciphertext bytes may be written at
-     * this stage.
-     */
-    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
-        handleErrors();
-    ciphertext_len += len;
+    for (int i = 0; i < ciphertext.size(); i++) {
 
-    /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
+        std::cout << "0x" << std::hex << (0xFF & static_cast<CryptoPP::byte>(ciphertext[i])) << " ";
+    }
 
-    return ciphertext_len;
-}
+    std::cout << std::endl << std::endl;
 
-int decrypt(unsigned char* ciphertext, int ciphertext_len, unsigned char* key,
-    unsigned char* iv, unsigned char* plaintext) {
-    EVP_CIPHER_CTX* ctx;
+    //
+    // Decrypt
+    //
+    CryptoPP::AES::Decryption aesDecryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
+    CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption(aesDecryption, iv);
 
-    int len;
+    CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink(decryptedtext));
+    stfDecryptor.Put(reinterpret_cast<const unsigned char*>(ciphertext.c_str()), ciphertext.size());
+    stfDecryptor.MessageEnd();
 
-    int plaintext_len;
-
-    /* Create and initialise the context */
-    if (!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
-
-    /*
-     * Initialise the decryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
-    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-        handleErrors();
-
-    /*
-     * Provide the message to be decrypted, and obtain the plaintext output.
-     * EVP_DecryptUpdate can be called multiple times if necessary.
-     */
-    if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-        handleErrors();
-    plaintext_len = len;
-
-    /*
-     * Finalise the decryption. Further plaintext bytes may be written at
-     * this stage.
-     */
-    if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
-        handleErrors();
-    plaintext_len += len;
-
-    /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
-
-    return plaintext_len;
-}
-
-void evp_test() {
-
-    /*
-     * Set up the key and iv. Do I need to say to not hard code these in a
-     * real application? :-)
-     */
-
-     /* A 256 bit key */
-    unsigned char* key = (unsigned char*)"testing123";
-
-    /* A 128 bit IV */
-    unsigned char* iv = (unsigned char*)"0123456789012345";
-
-    /* Message to be encrypted */
-    unsigned char* plaintext =
-        (unsigned char*)"The quick brown fox jumps over the lazy dog";
-
-    /*
-     * Buffer for ciphertext. Ensure the buffer is long enough for the
-     * ciphertext which may be longer than the plaintext, depending on the
-     * algorithm and mode.
-     */
-    unsigned char ciphertext[128] = { 0 };
-
-    /* Buffer for the decrypted text */
-    unsigned char decryptedtext[128] = { 0 };
-
-    int decryptedtext_len, ciphertext_len;
-
-    /* Encrypt the plaintext */
-    ciphertext_len = encrypt(plaintext, strlen((char*)plaintext), key, iv,
-        ciphertext);
-
-    /* Do something useful with the ciphertext here */
-    //printf("Ciphertext is:\n");
-    //BIO_dump_fp(stdout, (const char*)ciphertext, ciphertext_len);
-
-    /* Decrypt the ciphertext */
-    decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
-        decryptedtext);
-
-    /* Add a NULL terminator. We are expecting printable text */
-    decryptedtext[decryptedtext_len] = '\0';
-
-    /* Show the decrypted text */
-    printf("Decrypted text is:\n");
-    printf("%s\n", decryptedtext);
-
-}
-
-void rsa_test() {
-
-
-
-}
-
-int main(void) {
-    
-    rsa_test();
+    //
+    // Dump Decrypted Text
+    //
+    std::cout << "Decrypted Text: " << std::endl;
+    std::cout << decryptedtext;
+    std::cout << std::endl << std::endl;
 
     return 0;
 }
